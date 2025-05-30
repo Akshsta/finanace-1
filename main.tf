98cbc75bfd7681d1f1436ed9f76cb4acd998098f
Akshsta <akshatafansekar17@gmail.com>
2025-05-31 01:59:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "finance1_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["44.201.199.111/0"]
+    cidr_blocks = ["44.201.199.111/32"]
   }
 
   ingress {

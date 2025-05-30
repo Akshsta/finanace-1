657ae75fda443b409595f8d0ce96b6e3ccfd370a
Akshsta <akshatafansekar17@gmail.com>
2025-05-31 00:00:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_internet_gateway" "finance1_igw" {
 # Subnet
 resource "aws_subnet" "finance1_subnet" {
   vpc_id                  = aws_vpc.finance1_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }

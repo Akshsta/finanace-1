e7021eb0b1366cc3ea4dfd70b55393e210c76bf7
Akshsta <akshatafansekar17@gmail.com>
2025-05-30 23:50:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
 
 # Create a VPC
 resource "aws_vpc" "finance1_vpc" {

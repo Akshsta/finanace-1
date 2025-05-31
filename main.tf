f2fa385cc4f5bd287761eaad41bc04472bbc86e2
Akshsta <akshatafansekar17@gmail.com>
2025-06-01 00:52:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "finance-terraform-state-bucket"   # Replace with your S3 bucket
-    key            = "finance-me/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"                   # For state locking (optional but recommended)
-  }
-}
-
-
 provider "aws" {
   region = "us-east-1"
 }
